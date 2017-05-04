a085b8190c768212e922c003b4d784282ba2b0b7
wu_jiatian <u0024window@gmail.com>
2017-05-04 20:54:36 +0800

    Add files via upload
    up

--- replacefile.sh
+++ replacefile.sh
@@ -1,13 +1,19 @@
 # 第一个参数是源地址 第二个是目的地址 如果只有一个参数 源地址默认为当前地址
-if [ -z $2 ];then
-    source='.'
-    dest=$1
-else
-    source=$1
-    dest=$2
-fi
-# 复制的总文件数
-total=0
+# error tips 
+err () {
+    echo "\033[31merror:$1\033[0m"
+}
+
+# 路径错误
+com () {
+    `ls $1 >/dev/null 2>&1`
+    if [ 0 -ne $? ];then
+        err "path $1"
+        exit 1
+    fi
+}
+
+# copy 文件
 copyFile () {
     local sourcePath=$1
     local name=${sourcePath##*/}
@@ -28,6 +34,8 @@ copyFile () {
             fi
         done
 }
+
+# 递归 遍历
 lsp () {
     local spath=$1
     if [ -d "$1" ];then
@@ -43,7 +51,26 @@ lsp () {
         done
     fi
 }
+# begin ==================================================
+if [ -z "$1" ];then
+    err "至少一个参数: [source] [dest]"
+    exit 1
+fi
+if [ -z $2 ];then
+    source='.'
+    dest=$1
+    com $1
+
+else
+    source=$1
+    dest=$2
+    com $1
+    com $2
+fi
+# 复制的总文件数
+total=0
+
 IFS=$'\n'
 lsp $source
 # 输出总文件数
-echo "copy file number is $total"
+echo "copy file number is \033[35m$total\033[0m"
